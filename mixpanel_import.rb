84c9648dba2bb5a09de8068bb81c8e2bf4115246
Connor McEwen <connor.mcewen@gmail.com>
2015-06-24 16:22:26 -0400

    fix days

--- mixpanel_import.rb
+++ mixpanel_import.rb
@@ -8,7 +8,7 @@ require 'pp'
 @redis = Redis.new(:host => ENV['LOGSTASH_URI'], :port => 6379)
 
 @client = Mixpanel::Client.new(api_key: ENV['MIXPANEL_API'], api_secret: ENV['MIXPANEL_SECRET'])
-@data = @client.request('export', from_date: (Date.today-35).to_s, to_date: (Date.today-1).to_s)
+@data = @client.request('export', from_date: (Date.today-1).to_s, to_date: (Date.today-1).to_s)
 
 @data.each do |line| 
 	@redis.lpush "logstash", Hash["mixpanel_fields", 
